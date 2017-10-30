b0bb8d441b8e5a89e4d17dfd6f0e69c42463fa17
Kelly Ng <kellyng87@gmail.com>
2017-10-29 23:01:39 -0700

    make mobile icons show

---
+++ components/UdaciStepper.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function UdaciStepper () {
+  return (
+    <View>
+      <Text>
+        UdaciStepper
+      </Text>
+    </View>
+  )
+}
