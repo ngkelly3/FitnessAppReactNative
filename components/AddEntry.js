44c36993904cc82f52b4b12542601508cdfb9f12
Kelly Ng <kellyng87@gmail.com>
2017-10-30 22:20:17 -0700

    add alreadylogged view

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -76,8 +78,31 @@ export default class AddEntry extends Component {
     // Clear local notification
   }
 
+  reset = () => {
+    const key = timeToString()
+
+    // Update Redux
+    // Route to home
+    // Update "DB"
+  }
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+       return (
+         <View>
+           <Ionicons
+             name="ios-happy-outline"
+             size={100} />
+             <Text>
+               You already logged your information for today.
+             </Text>
+             <TextButton onPress={this.reset}>
+               Reset
+             </TextButton>
+         </View>
+       )
+     }
     return (
       <View>
           <DateHeader date={(new Date()).toLocaleDateString()} />
