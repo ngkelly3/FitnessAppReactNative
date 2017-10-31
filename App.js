b8f92916105e478fb2abf6f78056ba39f6d95460
Kelly Ng <kellyng87@gmail.com>
2017-10-30 22:09:39 -0700

    set up slider and stepper component

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import {  View  } from 'react-native';
+import {  View, StyleSheet, Slider, Text  } from 'react-native';
 import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
+
+  state = {
+    value: 0
+  }
+
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <AddEntry />
       </View>
-    );
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: "stretch",
+    justifyContent: "center"
+  }
+})
--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { FontAwesome, Entypo } from '@expo/vector-icons'
 
-export default function UdaciStepper () {
+export default function UdaciStepper ({ max, unit, step, value, onIncrement, onDecrement }) {
   return (
     <View>
-      <Text>
-        UdaciStepper
-      </Text>
+      <TouchableOpacity onPress={onDecrement}>
+        <FontAwesome name='minus' size={30} color={'black'} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onIncrement}>
+        <FontAwesome name='plus' size={30} color={'black'} />
+      </TouchableOpacity>
+      <View>
+        <Text>
+          {value}
+        </Text>
+        <Text>
+          {unit}
+        </Text>
+      </View>
     </View>
   )
 }
